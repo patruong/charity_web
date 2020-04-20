a1eeb3f82b1bc0a7aabc718507764d5ef4f739da
unknown <p.truong@esynergetics.se>
2020-04-20 18:39:08 +0200

    init

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-
-const routes: Routes = [];
+import { BlogComponent } from './components/blog/blog.component';
+const routes: Routes = [
+{ path: 'blog', component: BlogComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
